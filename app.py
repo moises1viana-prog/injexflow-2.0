cee288544b032a4d3fe599c899c8de815300db4b
moises1viana-prog <moises1viana@gmail.com>
2026-03-20 19:05:24 -0300

    Create initial Python application setup for InjexFlow

---
+++ app.py
@@ -0,0 +1,12 @@
+# InjexFlow
+
+# Initial Python application setup
+
+# Import necessary libraries
+import os
+import sys
+
+# Define main entry point
+if __name__ == '__main__':
+    print("InjexFlow Application Start")
+    # Add your application logic here
